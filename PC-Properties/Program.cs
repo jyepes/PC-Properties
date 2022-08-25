4662c6609befe8b9afbf402ce677da6bc77e1b6b
Julio C. Yepes <julio.yepes@3ysolutions.com.co>
2022-08-25 14:46:08 -0500

    1er commit

--- PC-Properties/Program.cs
+++ PC-Properties/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,33 @@ namespace PC_Properties
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine($"MAC Address: {GetMACAddress()}");
+            Console.WriteLine($"Host Name: {GetHostName()}");
+            Console.WriteLine($"IP Address: {GetIpAddress()}");
+            Console.ReadLine();
         }
+        private static string GetMACAddress()
+        {
+            return NetworkInterface
+                .GetAllNetworkInterfaces()
+                .Where(nic => nic.OperationalStatus == OperationalStatus.Up && nic.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                .Select(nic => nic.GetPhysicalAddress().ToString())
+                .FirstOrDefault();
+        }
+
+        private static string GetHostName()
+        {
+            return Dns.GetHostName();
+        }
+
+        private static string GetIpAddress()
+        {
+            string HostName = Dns.GetHostName();
+            IPAddress[] ipaddress = Dns.GetHostAddresses(HostName);
+
+            return ipaddress.Where(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).FirstOrDefault().ToString();
+        }
+
     }
 }
